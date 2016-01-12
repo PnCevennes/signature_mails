ef0b05c8cfe39af5bdfd15a02cfb627decaa8041
fdonfi <frederic.fidon@cevennes-parcnational.fr>
2016-01-12 12:07:59 +0100

    ajout logo reserve biosphere + cfg lien parc + nettoyage template

--- app.js
+++ app.js
@@ -12,18 +12,19 @@ app.controller('mainCtrl', function(){
         var prenom_f = this.prenom[0].toUpperCase() + this.prenom.slice(1).toLowerCase();
         var fonction_f= this.fonction[0].toUpperCase() + this.fonction.slice(1).toLowerCase();
         var output = TEMPLATE
-            .replace(/__CNF_PARC__/, CONFIG.parc)
-            .replace(/__CNF_LOGO__/, CONFIG.logo)
-            .replace(/__CNF_POINTS__/, CONFIG.points)
-            .replace(/__CNF_FB__/, CONFIG.facebook)
-            .replace(/__CNF_TW__/, CONFIG.twitter)
-            .replace(/__CNF_YT__/, CONFIG.youtube)
-            .replace(/__CNF_PT__/, CONFIG.pinterest)
-            .replace(/__CNF_IG__/, CONFIG.instagram)
-            .replace(/__PRENOM__/, prenom_f)                                
-            .replace(/__NOM__/, this.nom.toUpperCase())                                
-            .replace(/__FONCTION__/, fonction_f)                                
-            .replace(/__TEL__/, this.tel);                                
+            .replace(/__CNF_PARC__/g, CONFIG.parc)
+            .replace(/__CNF_LIEN_PARC__/g, CONFIG.lien_parc)
+            .replace(/__CNF_LOGO__/g, CONFIG.logo)
+            .replace(/__CNF_POINTS__/g, CONFIG.points)
+            .replace(/__CNF_FB__/g, CONFIG.facebook)
+            .replace(/__CNF_TW__/g, CONFIG.twitter)
+            .replace(/__CNF_YT__/g, CONFIG.youtube)
+            .replace(/__CNF_PT__/g, CONFIG.pinterest)
+            .replace(/__CNF_IG__/g, CONFIG.instagram)
+            .replace(/__PRENOM__/g, prenom_f)                                
+            .replace(/__NOM__/g, this.nom.toUpperCase())                                
+            .replace(/__FONCTION__/g, fonction_f)                                
+            .replace(/__TEL__/g, this.tel);                                
         return output
     });
 
